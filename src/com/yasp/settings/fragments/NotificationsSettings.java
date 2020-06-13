8861391fc8ce516dfb0e9d31c53e7f1342362929
Ido Ben-Hur <idoybh2@gmail.com>
2020-06-13 03:31:47 +0300

    YASP: Add DND & Rate settings to flash on call [3/3]

--- src/com/yasp/settings/fragments/NotificationsSettings.java
+++ src/com/yasp/settings/fragments/NotificationsSettings.java
@@ -15,7 +15,9 @@
  */
 package com.yasp.settings.fragments;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.provider.Settings;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceGroup;
@@ -31,12 +33,23 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.R;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.yasp.settings.preferences.CustomSeekBarPreference;
+import com.yasp.settings.preferences.SystemSettingListPreference;
+import com.yasp.settings.preferences.SystemSettingSwitchPreference;
 import com.yasp.settings.Utils;
 
 @SearchIndexable
-public class NotificationsSettings extends SettingsPreferenceFragment {
+public class NotificationsSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
 
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+    private static final String PREF_FLASH_ON_CALL = "flashlight_on_call";
+    private static final String PREF_FLASH_ON_CALL_DND = "flashlight_on_call_ignore_dnd";
+    private static final String PREF_FLASH_ON_CALL_RATE = "flashlight_on_call_rate";
+
+    private SystemSettingListPreference mFlashOnCall;
+    private SystemSettingSwitchPreference mFlashOnCallIgnoreDND;
+    private CustomSeekBarPreference mFlashOnCallRate;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -49,6 +62,43 @@ public class NotificationsSettings extends SettingsPreferenceFragment {
         if (!Utils.isVoiceCapable(getActivity())) {
             prefScreen.removePreference(incallVibCategory);
         }
+
+        mFlashOnCallRate = (CustomSeekBarPreference)
+                findPreference(PREF_FLASH_ON_CALL_RATE);
+        int value = Settings.System.getInt(resolver,
+                Settings.System.FLASHLIGHT_ON_CALL_RATE, 1);
+        mFlashOnCallRate.setValue(value);
+        mFlashOnCallRate.setOnPreferenceChangeListener(this);
+
+        mFlashOnCallIgnoreDND = (SystemSettingSwitchPreference)
+                findPreference(PREF_FLASH_ON_CALL_DND);
+        value = Settings.System.getInt(resolver,
+                Settings.System.FLASHLIGHT_ON_CALL, 0);
+        mFlashOnCallIgnoreDND.setVisible(value > 1);
+        mFlashOnCallRate.setVisible(value != 0);
+
+        mFlashOnCall = (SystemSettingListPreference)
+                findPreference(PREF_FLASH_ON_CALL);
+        mFlashOnCall.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mFlashOnCall) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, value);
+            mFlashOnCallIgnoreDND.setVisible(value > 1);
+            mFlashOnCallRate.setVisible(value != 0);
+            return true;
+        } else if (preference == mFlashOnCallRate) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL_RATE, value);
+            return true;
+        }
+        return false;
     }
 
     @Override
