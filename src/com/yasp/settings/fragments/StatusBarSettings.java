0fc1986159c30088476d8d34469dad12e01ef577
acras01 <acras1@gmail.com>
2021-11-18 17:51:56 +0530

    YASP: Allow toggling combined signal icons [2/2]
    * @jhonboy121: remove preference controller usaeg and use the default value for switch from sysui resource
    idoybh (YAAP): Good ol' java
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: idoybh <idoybh2@gmail.com>

--- src/com/yasp/settings/fragments/StatusBarSettings.java
+++ src/com/yasp/settings/fragments/StatusBarSettings.java
@@ -16,24 +16,14 @@
 package com.yasp.settings.fragments;
 
 import android.content.ContentResolver;
-import android.content.Context;
-import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
-import android.text.format.DateFormat;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
 
 import androidx.preference.Preference;
-import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.PreferenceCategory;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
@@ -41,24 +31,26 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.yasp.settings.preferences.SecureSettingSwitchPreference;
 import com.yasp.settings.preferences.SystemSettingListPreference;
 import com.yasp.settings.preferences.SystemSettingMasterSwitchPreference;
 import com.yasp.settings.preferences.SystemSettingSwitchPreference;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @SearchIndexable
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String SYSTEMUI_PACKAGE = "com.android.systemui";
+    private static final String CONFIG_RESOURCE_NAME = "flag_combined_status_bar_signal_icons";
+
     private static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+    private static final String COBINED_STATUSBAR_ICONS = "show_combined_status_bar_signal_icons";
     private static final String BATTERY_STYLE = "status_bar_battery_style";
     private static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
     private static final String SHOW_BATTERY_PERCENT_INSIDE = "status_bar_show_battery_percent_inside";
 
     private SystemSettingMasterSwitchPreference mNetTrafficState;
+    private SecureSettingSwitchPreference mCombinedIcons;
     private SystemSettingListPreference mBatteryStyle;
     private SystemSettingSwitchPreference mBatteryPercent;
     private SystemSettingSwitchPreference mBatteryPercentInside;
@@ -76,6 +68,27 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         boolean enabled = Settings.System.getInt(resolver,
                 Settings.System.NETWORK_TRAFFIC_STATE, 0) == 1;
         mNetTrafficState.setChecked(enabled);
+
+        mCombinedIcons = (SecureSettingSwitchPreference)
+                findPreference(COBINED_STATUSBAR_ICONS);
+        Resources sysUIRes = null;
+        boolean def = false;
+        int resId = 0;
+        try {
+            sysUIRes = getActivity().getPackageManager()
+                    .getResourcesForApplication(SYSTEMUI_PACKAGE);
+        } catch (Exception ignored) {
+            // If you don't have system UI you have bigger issues
+        }
+        if (sysUIRes != null) {
+            resId = sysUIRes.getIdentifier(
+                    CONFIG_RESOURCE_NAME, "bool", SYSTEMUI_PACKAGE);
+            if (resId != 0) def = sysUIRes.getBoolean(resId);
+        }
+        enabled = Settings.Secure.getInt(resolver,
+                COBINED_STATUSBAR_ICONS, def ? 1 : 0) == 1;
+        mCombinedIcons.setChecked(enabled);
+        mCombinedIcons.setOnPreferenceChangeListener(this);
 
         mBatteryPercentInside = (SystemSettingSwitchPreference)
                 findPreference(SHOW_BATTERY_PERCENT_INSIDE);
@@ -106,7 +119,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
                     Settings.System.NETWORK_TRAFFIC_STATE, enabled ? 1 : 0);
             return true;
         } else if (preference == mBatteryStyle) {
-            int value = Integer.valueOf((String) objValue);
+            int value = Integer.parseInt((String) objValue);
             int index = mBatteryStyle.findIndexOfValue((String) objValue);
             mBatteryStyle.setSummary(mBatteryStyle.getEntries()[index]);
             Settings.System.putIntForUser(resolver,
@@ -118,6 +131,11 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(resolver,
                     SHOW_BATTERY_PERCENT, enabled ? 1 : 0);
             mBatteryPercentInside.setEnabled(enabled);
+            return true;
+        } else if (preference == mCombinedIcons) {
+            boolean enabled = (boolean) objValue;
+            Settings.Secure.putInt(resolver,
+                    COBINED_STATUSBAR_ICONS, enabled ? 1 : 0);
             return true;
         }
         return false;
