3d23521fc058fe053337adaf3ab6d16c3682dd6f
Ido Ben-Hur <idoybh2@gmail.com>
2022-09-30 04:24:59 +0300

    Revert "YASP: allow toggling location privacy indicator [2/2]"
    This reverts commit 471991961b500bd92783e6758aa92198728c9456.

--- src/com/yasp/settings/fragments/StatusBarSettings.java
+++ src/com/yasp/settings/fragments/StatusBarSettings.java
@@ -21,7 +21,6 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.provider.DeviceConfig;
 import android.provider.Settings;
 import android.text.format.DateFormat;
 import android.text.TextUtils;
@@ -56,10 +55,8 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String SYSTEMUI_PACKAGE = "com.android.systemui";
-    private static final String LOCATION_DEVICE_CONFIG = "location_indicators_enabled";
 
     private static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
-    private static final String LOCATION_INDICATOR = "enable_location_privacy_indicator";
     private static final String CLOCK_POSITION = "statusbar_clock_position";
     private static final String BATTERY_STYLE = "status_bar_battery_style";
     private static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
@@ -84,13 +81,6 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
                 NETWORK_TRAFFIC_STATE, 0) == 1;
         mNetTrafficState.setChecked(enabled);
         updateNetTrafficSummary(enabled);
-
-        SecureSettingSwitchPreference locationIndicator = findPreference(LOCATION_INDICATOR);
-        boolean def = DeviceConfig.getBoolean(DeviceConfig.NAMESPACE_PRIVACY,
-                LOCATION_DEVICE_CONFIG, false);
-        locationIndicator.setDefaultValue(def);
-        locationIndicator.setChecked(Settings.Secure.getInt(resolver,
-                LOCATION_INDICATOR, def ? 1 : 0) == 1);
 
         mClockPosition = findPreference(CLOCK_POSITION);
         int value = Settings.System.getIntForUser(resolver,
