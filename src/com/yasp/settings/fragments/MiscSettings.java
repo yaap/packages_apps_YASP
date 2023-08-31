5247a6fb1301c393a8f1ac3393cf4caa9ce39b9a
Ido Ben-Hur <idoybh2@gmail.com>
2023-08-31 13:20:40 +0300

    YASP: Add a backup transport selector

--- src/com/yasp/settings/fragments/MiscSettings.java
+++ src/com/yasp/settings/fragments/MiscSettings.java
@@ -15,19 +15,93 @@
  */
 package com.yasp.settings.fragments;
 
+import android.app.backup.IBackupManager;
+import android.app.backup.SelectBackupTransportCallback;
+import android.content.ComponentName;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.UserHandle;
+import android.widget.Toast;
 
 import com.android.internal.logging.nano.MetricsProto;
-
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import java.util.Arrays;
+
 @SearchIndexable
-public class MiscSettings extends DashboardFragment {
+public class MiscSettings extends DashboardFragment implements
+        OnPreferenceChangeListener {
 
     private static final String TAG = "MiscSettings";
+
+    private static final String TRANSPORT_SELECTOR_KEY = "transport_selector";
+
+    private IBackupManager mBackupManager;
+    private Toast mToast;
+    private ListPreference mTransportSelector;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        mBackupManager = IBackupManager.Stub.asInterface(
+                ServiceManager.getService(Context.BACKUP_SERVICE));
+        mTransportSelector = findPreference(TRANSPORT_SELECTOR_KEY);
+        try {
+            final String current = mBackupManager.getCurrentTransportForUser(UserHandle.USER_SYSTEM);
+            final String[] transports = mBackupManager.listAllTransportsForUser(UserHandle.USER_SYSTEM);
+            final String[] entries = new String[transports.length];
+            for (int i = 0; i < transports.length; i++) {
+                final String[] segments = transports[i].split("/");
+                if (segments.length < 2) {
+                    entries[i] = transports[i];
+                    continue;
+                }
+                final String[] activitySegments = segments[1].split("\\.");
+                entries[i] = activitySegments[activitySegments.length - 1] + " (" + segments[0] + ")";
+            }
+            mTransportSelector.setEntries(entries);
+            mTransportSelector.setEntryValues(transports);
+            mTransportSelector.setValue(current);
+            mTransportSelector.setSummary(mTransportSelector.getEntry());
+            mTransportSelector.setOnPreferenceChangeListener(this);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            mTransportSelector.setVisible(false);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mTransportSelector) {
+            final String value = (String) objValue;
+            try {
+                final String old = mBackupManager.selectBackupTransportForUser(
+                        UserHandle.USER_SYSTEM, value);
+                if (old == null) {
+                    showToast(R.string.failure);
+                    return false;
+                }
+                int index = mTransportSelector.findIndexOfValue(value);
+                mTransportSelector.setSummary(mTransportSelector.getEntries()[index]);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+                showToast(R.string.failure);
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
 
     @Override
     protected int getPreferenceScreenResId() {
@@ -42,6 +116,12 @@ public class MiscSettings extends DashboardFragment {
     @Override
     protected String getLogTag() {
         return TAG;
+    }
+
+    private synchronized void showToast(int msgId) {
+        if (mToast != null) mToast.cancel();
+        mToast = Toast.makeText(getContext(), msgId, Toast.LENGTH_LONG);
+        mToast.show();
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
