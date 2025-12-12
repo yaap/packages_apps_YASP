30b668de834c96f839f899b4f36f74ce27a458d0
Ido Ben-Hur <idoybh2@gmail.com>
2025-12-12 22:50:57 +0200

    YASP: CustomSeekBarPreference: Adapt to A16/QPR2

--- src/com/yasp/settings/preferences/CustomSeekBarPreference.java
+++ src/com/yasp/settings/preferences/CustomSeekBarPreference.java
@@ -133,10 +133,6 @@ public class CustomSeekBarPreference extends Preference implements Slider.OnChan
             mValue = mMinValue;
         }
 
-        Context materialContext = new ContextThemeWrapper(context,
-                com.google.android.material.R.style.Theme_MaterialComponents_DayNight);
-        mSlider = new Slider(materialContext, attrs);
-
         setLayoutResource(R.layout.preference_custom_seekbar);
     }
 
@@ -157,25 +153,8 @@ public class CustomSeekBarPreference extends Preference implements Slider.OnChan
     @Override
     public void onBindViewHolder(PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
-        try
-        {
-            // move our seekbar to the new view we've been given
-            ViewParent oldContainer = mSlider.getParent();
-            ViewGroup newContainer = (ViewGroup) holder.findViewById(R.id.seekbar);
-            if (oldContainer != newContainer) {
-                // remove the seekbar from the old view
-                if (oldContainer != null) {
-                    ((ViewGroup) oldContainer).removeView(mSlider);
-                }
-                // remove the existing seekbar (there may not be one) and add ours
-                newContainer.removeAllViews();
-                newContainer.addView(mSlider, ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT);
-            }
-        } catch (Exception ex) {
-            Log.e(TAG, "Error binding view", ex);
-        }
 
+        mSlider = (Slider) holder.findViewById(R.id.slider);
         mSlider.setValueTo(mMaxValue);
         mSlider.setValueFrom(mMinValue);
         mSlider.setValue(mValue);
@@ -187,26 +166,6 @@ public class CustomSeekBarPreference extends Preference implements Slider.OnChan
         } else {
             Log.w(TAG, "Step size is zero or invalid: " + mInterval);
         }
-
-        // Set up slider color
-        mSlider.setTrackActiveTintList(getContext().getColorStateList(
-                com.android.settingslib.widget.preference.slider.R.color
-                .settingslib_expressive_color_slider_track_active));
-        mSlider.setTrackInactiveTintList(getContext().getColorStateList(
-                com.android.settingslib.widget.preference.slider.R.color
-                .settingslib_expressive_color_slider_track_inactive));
-        mSlider.setThumbTintList(getContext().getColorStateList(
-                com.android.settingslib.widget.preference.slider.R.color
-                .settingslib_expressive_color_slider_thumb));
-        mSlider.setHaloTintList(getContext().getColorStateList(
-                com.android.settingslib.widget.preference.slider.R.color
-                .settingslib_expressive_color_slider_halo));
-        mSlider.setTickActiveTintList(getContext().getColorStateList(
-                com.android.settingslib.widget.preference.slider.R.color
-                .settingslib_expressive_color_slider_track_active));
-        mSlider.setTickInactiveTintList(getContext().getColorStateList(
-                com.android.settingslib.widget.preference.slider.R.color
-                .settingslib_expressive_color_slider_track_inactive));
 
         // Set up slider size
         if (SettingsThemeHelper.isExpressiveTheme(getContext())) {
